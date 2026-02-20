588be32e056ea7aff0119ba85d6eadcfb9ffbb6d
Siddhi Salunke <siddhisam714@gmail.com>
2026-02-20 23:08:16 +0530

    Create HelloStudent.java

---
+++ Practical_01/HelloStudent.java
@@ -0,0 +1,8 @@
+public class HelloStudent {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Student Name: Siddhi Salunke");
+        System.out.println("Class: SYIT");
+        System.out.println("Roll No: 24302A0061");
+    }
+}
